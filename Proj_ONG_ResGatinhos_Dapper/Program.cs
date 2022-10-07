bd34268577013a467881606b7cbab69891a583a5
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-10-07 20:45:38 -0300

    Adicionar arquivos de projeto.

---
+++ Proj_ONG_ResGatinhos_Dapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proj_ONG_ResGatinhos_Dapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
