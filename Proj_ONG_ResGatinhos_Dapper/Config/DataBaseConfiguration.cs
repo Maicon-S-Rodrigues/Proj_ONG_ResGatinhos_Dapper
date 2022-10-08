f60afb5960268da70a1df7ce7565360cb85afefa
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-10-07 21:33:25 -0300

    Adicionado: Parte da configuracao de caminho para conexao com o banco de dados via arquivo json

---
+++ Proj_ONG_ResGatinhos_Dapper/Config/DataBaseConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;// ///
+using System;
+using System.Collections.Generic;
+using System.IO;///
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_ONG_ResGatinhos_Dapper.Config
+{
+    public class DataBaseConfiguration
+    {
+        public static IConfigurationRoot configuration { get; set; }//
+
+        public static string Get()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())///
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            configuration = builder.Build();
+            string _conn = configuration["ConnectionStrings:DefaultConnection"];
+
+            return _conn;
+        }
+    }
+}
