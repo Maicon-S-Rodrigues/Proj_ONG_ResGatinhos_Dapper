faeab218ffff336780ff47f7ab947b70e3a60166
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-10-07 22:50:10 -0300

    Adicionado: Insert e SelectAll de Adotante em PessoaServices

---
+++ Proj_ONG_ResGatinhos_Dapper/Services/PessoaServices.cs
@@ -0,0 +1,30 @@
+﻿using Proj_ONG_ResGatinhos_Dapper.Model;
+using Proj_ONG_ResGatinhos_Dapper.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_ONG_ResGatinhos_Dapper.Services
+{
+    public class PessoaServices
+    {
+        private IPessoaRepository _PessoaRepository;
+
+        public PessoaServices()
+        {
+            _PessoaRepository = new PessoaRepository();
+        }
+
+        public bool Add (Pessoa pessoa)
+        {
+            return _PessoaRepository.Add(pessoa);
+        }
+
+        public List<Pessoa> GetAll()
+        {
+            return _PessoaRepository.GetAll();
+        }
+    }
+}
