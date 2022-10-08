783248d53c6ed520c0a502af521c916936931464
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-10-07 22:43:26 -0300

    Adicionado: Insert de Adotante

---
+++ Proj_ONG_ResGatinhos_Dapper/Repository/PessoaRepository.cs
@@ -0,0 +1,71 @@
+﻿using Dapper;
+using Proj_ONG_ResGatinhos_Dapper.Config;
+using Proj_ONG_ResGatinhos_Dapper.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_ONG_ResGatinhos_Dapper.Repository
+{
+    public class PessoaRepository : IPessoaRepository
+    {
+        private readonly string _conn;
+
+        public PessoaRepository()
+        {
+            _conn = DataBaseConfiguration.Get();
+        }
+
+
+        public bool Add(Pessoa pessoa)
+        {
+            bool result = false;
+
+            using (var db = new SqlConnection(_conn))
+            {
+                db.Open();
+                db.Execute(Pessoa.Insert, pessoa);
+                result = true;
+            }
+            return result;
+        }
+
+        public Pessoa Get(string cpf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pessoa> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateDataNascimento(string cpf, DateTime dataNascimento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateEndereco(string cpf, string cidade, string estado, string bairro, string rua, int numero, string complemento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateNome(string cpf, string nome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateSexo(string cpf, string sexo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateTelefone(string cpf, string telefone)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
