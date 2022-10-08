05f065185aee0e26dc54c58b1ceef537e2ba147e
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-10-08 17:26:27 -0300

    Adicionado: Interface Repository para "Animal" IAnimalRepository com seus contratos/assinaturas

---
+++ Proj_ONG_ResGatinhos_Dapper/Repository/IAnimalRepository.cs
@@ -0,0 +1,22 @@
+﻿using Proj_ONG_ResGatinhos_Dapper.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_ONG_ResGatinhos_Dapper.Repository
+{
+    internal interface IAnimalRepository
+    {
+        bool Add(Animal animal);
+        List<Animal> GetAllAdotados();
+        List<Animal> GetAllDisponiveis();
+        public bool Exists(string chip);
+        Pessoa Get(string chip);
+        bool UpdateFamilia(string chip, string familia);
+        bool UpdateRaca(string chip, string raca);
+        bool UpdateSexo(string chip, string sexo);
+        bool UpdateNome(string chip, string nome);
+    }
+}
