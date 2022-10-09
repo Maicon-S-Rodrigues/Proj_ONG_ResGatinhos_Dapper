2d219ed9a8bfa5ec75b19616be822b92cb7c94a5
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-10-08 22:14:41 -0300

    Adicionado: ToString de "Adota" e propiedades para classe "Adota"

---
+++ Proj_ONG_ResGatinhos_Dapper/Model/Adota.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_ONG_ResGatinhos_Dapper.Model
+{
+    public class Adota
+    {
+        #region Constant - SQL Commands
+
+        #endregion
+        public string PessoaCpf { get; set; }
+        public string AnimalChip { get; set; }
+        public string PessoaNome { get; set; }
+        public string AnimalFamilia { get; set; }
+        public string AnimalNome { get; set; }
+        public string AnimalRaca { get; set; }
+        public string AnimalSituacao { get; set; }
+
+        public Adota(){}
+
+        public override string ToString()
+        {
+            return " | CPF: " + this.PessoaCpf + " | Nome: " + this.PessoaNome + " |" +
+                 "\n | CHIP do Pet: " + this.AnimalChip + " | Família Animal: " + this.AnimalFamilia + " |" +
+                 "\n | Raça: " + this.AnimalRaca + " | Nome do Pet: |" + this.AnimalNome + " | Situação: " + this.AnimalSituacao + " |" +
+                 "\n___________________________________________________________________________________________________\n";
+        }
+    }
+}
